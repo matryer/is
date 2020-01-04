aa03e8bbf5de7d2a7aaa7c81cc1465749f55da8c
Lucas Bremgartner <lucas@bremis.ch>
2020-01-04 20:10:56 +0100

    Run tests in CI

--- is-1.7_test.go
+++ is-1.7_test.go
@@ -8,6 +8,15 @@ import (
 	"testing"
 )
 
+// TestSubtests ensures subtests work as expected.
+// https://github.com/matryer/is/issues/1
+func TestSubtests(t *testing.T) {
+	t.Run("sub1", func(t *testing.T) {
+		is := New(t)
+		is.Equal(1+1, 2)
+	})
+}
+
 func TestHelper(t *testing.T) {
 	tests := []struct {
 		name             string
